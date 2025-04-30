eaa528eff1fb8f0c64f82e3af756dd1b6404e6e6
NatKW <nath.burgade@gmail.com>
2025-04-30 16:14:10 +0200

    Vagrantfile modified --> ConfigMap&Deploy

--- p2/scripts/worker_setup.sh
+++ p2/scripts/worker_setup.sh
@@ -7,11 +7,11 @@ sleep 10  # s'assurer que le token est bien présent
 sudo apt update -y
 sudo apt install -y curl net-tools
 
-# Installation de K3s en tant que worker
+# Installation de K3s pour le worker
 export INSTALL_K3S_EXEC="--node-ip=$WORKER_IP --flannel-iface=eth1" 
 export K3S_URL="https://$MASTER_IP:6443"
 export K3S_TOKEN_FILE="/vagrant/node-token"
 curl -sfL https://get.k3s.io | sh -
 
-# Alias kubectl (au cas où pour debug)
+# Alias kubectl
 echo "alias k='kubectl'" | sudo tee /etc/profile.d/00-aliases.sh
